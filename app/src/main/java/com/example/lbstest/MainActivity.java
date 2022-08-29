954466919e69416d0d199366abe419c65d404647
Peng <1871850810@qq.com>
2022-08-29 15:46:48 +0800

    second commit

--- app/src/main/java/com/example/lbstest/MainActivity.java
+++ app/src/main/java/com/example/lbstest/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.lbstest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.test_bt).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.test_bt)
+        {
+            finish();
+        }
+
     }
 }
